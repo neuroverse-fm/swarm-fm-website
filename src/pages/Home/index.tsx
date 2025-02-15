b37c9c0962d9ea9ac231cbd1fde20148b4223997
KTrain5369 <git@ktrain5369.me>
2025-02-15 12:01:40 +1000

    proper favicon

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -5,6 +5,9 @@ import { RADIO_TITLE } from '../../consts';
 export function Home() {
   return (
     <Layout title="Home">
+      <div class="items-center justify-center text-center">
+        <img src="/tutel.png" class="max-h-48" />
+      </div>
       <h1 class="text-3xl font-bold text-center">{RADIO_TITLE}</h1>
       <p class="mt-4 text-center">The Swarm's number one, karaoke radio player!</p>
     </Layout>
