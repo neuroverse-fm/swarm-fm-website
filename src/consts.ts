5608246e336b144c1527817ab0ad5ab9276bf025
KTrain <69028025+KTrain5169@users.noreply.github.com>
2025-03-20 08:13:42 +1000

    Update to new livestream yt code

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,3 @@
 export const RADIO_TITLE_PLAIN = "Swarm FM";
 export const RADIO_TITLE = `♫ The ${RADIO_TITLE_PLAIN} ♫`;
-export const YOUTUBE_STREAM = "https://www.youtube.com/embed/thCiTnOzkOM";
+export const YOUTUBE_STREAM = "https://www.youtube.com/embed/L8kqGez16dc";
