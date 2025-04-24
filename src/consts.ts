c568902a0e2ae6cfed349d2cc76d9561c3cac710
KTrain5369 <git@ktrain5369.me>
2025-04-24 12:54:24 +1000

    self-query for the livestream link

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,10 @@
 export const RADIO_TITLE_PLAIN = "Swarm FM";
 export const RADIO_TITLE = `♫ The ${RADIO_TITLE_PLAIN} ♫`;
-export const YOUTUBE_STREAM = "https://www.youtube.com/embed/L8kqGez16dc";
+
+async function selfQueryYouTubeStream() {
+    const res = await fetch('/api/livestream');
+    const data = await res.json()
+    return (data as any).embedUrl
+}
+
+export const YOUTUBE_STREAM = selfQueryYouTubeStream();
